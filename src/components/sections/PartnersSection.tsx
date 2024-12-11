63a59647999e3682f827b84fbdd789d88244bc9e
Teamified <teamified@Leonell-Cruz.local>
2024-12-11 23:34:08 +0800

    fix logo sizes

--- src/components/sections/PartnersSection.tsx
+++ src/components/sections/PartnersSection.tsx
@@ -46,13 +46,16 @@ const PartnersSection = (props: Type) => {
                 className={classNames(
                   flex
                     ? "h-10 md:h-[55px] flex-shrink-0 py-2"
-                    : "flex-shrink-0 h-[80px] md:h-[115px] lg:h-[161px] flex items-center justify-center px-6 md:px-11 lg:px-[65px]",
+                    : "flex-shrink-0 w-[165px] md:w-[216px] lg:w-[315px] h-[80px] md:h-[115px] lg:h-[161px] flex items-center justify-center mx-auto px-6 md:px-11 lg:px-[65px]",
                 )}
               >
                 <img
                   src={item.image.url}
                   alt="partner"
-                  className="object-contain h-full"
+                  className={classNames(
+                    "object-contain h-full max-h-[50px] max-w-[90px] md:max-h-[80px] md:max-w-[150px]",
+                    flex && "!max-w-max !max-h-max",
+                  )}
                 />
               </div>
             );
