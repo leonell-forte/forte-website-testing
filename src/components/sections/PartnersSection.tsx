6b8c450ec05e3472b51c9e1b04bab0ae00b93c67
Teamified <teamified@Leonell-Cruz.local>
2024-12-12 00:36:31 +0800

    fix partners section overflow

--- src/components/sections/PartnersSection.tsx
+++ src/components/sections/PartnersSection.tsx
@@ -32,34 +32,37 @@ const PartnersSection = (props: Type) => {
           {title}
         </p>
 
-        <div
-          className={classNames(
-            flex
-              ? "flex gap-12 overflow-scroll hide-scroll"
-              : "grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5",
-          )}
-        >
-          {partner.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className={classNames(
-                  flex
-                    ? "h-10 md:h-[55px] flex-shrink-0 py-2"
-                    : "flex-shrink-0 w-[165px] md:w-[216px] lg:w-[315px] h-[80px] md:h-[115px] lg:h-[161px] flex items-center justify-center mx-auto px-6 md:px-11 lg:px-[65px]",
-                )}
-              >
-                <img
-                  src={item.image.url}
-                  alt="partner"
+        <div className="w-full overflow-scroll px-5 hide-scroll">
+          <div
+            className={classNames(
+              flex
+                ? "flex gap-12 overflow-scroll hide-scroll"
+                : "grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5",
+              "w-fit mx-auto",
+            )}
+          >
+            {partner.map((item, index) => {
+              return (
+                <div
+                  key={index}
                   className={classNames(
-                    "object-contain h-full max-h-[50px] max-w-[90px] md:max-h-[80px] md:max-w-[150px]",
-                    flex && "!max-w-max !max-h-max",
+                    flex
+                      ? "h-10 md:h-[55px] flex-shrink-0 py-2"
+                      : "flex-shrink-0 w-[165px] md:w-[216px] lg:w-[315px] h-[80px] md:h-[115px] lg:h-[161px] flex items-center justify-center mx-auto px-6 md:px-11 lg:px-[65px]",
                   )}
-                />
-              </div>
-            );
-          })}
+                >
+                  <img
+                    src={item.image.url}
+                    alt="partner"
+                    className={classNames(
+                      "object-contain h-full max-h-[50px] max-w-[90px] md:max-h-[80px] md:max-w-[150px]",
+                      flex && "!max-w-max !max-h-max",
+                    )}
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
